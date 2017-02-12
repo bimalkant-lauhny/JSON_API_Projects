93fbfe9e3bfeb44e33a722ba15a2cc95b570d225
code_master5 <lauhny.bimalk@gmail.com>
2017-02-12 19:04:38 +0530

    fixed trigram display

--- public/javascripts/portfolio.js
+++ public/javascripts/portfolio.js
@@ -6,17 +6,24 @@ window.onload = function () {
     var topLinks = document.getElementsByClassName('top-links')[0];
     var nav = document.getElementsByTagName('nav')[0];
     var responsiveIco = document.getElementsByClassName('responsive-expand')[0];
+    var me = document.getElementsByClassName('creator')[0];
     
     document.getElementsByClassName('responsive-expand')[0].addEventListener('click', function () {
         
         if (topLinks.style.display === "none") {
-            topLinks.style.display = "block";
+            topLinks.style.display = "inline-block";
             responsiveIco.innerHTML = cross;
-            nav.style.height = "100%";
+            nav.style.height = "auto"
+            me.style.display = "none"
         } else {
             topLinks.style.display = "none";
             responsiveIco.innerHTML = trigram;
             nav.style.height = "80px";
+            me.style.display = "block"
         }
     });
+    
+    if (window.matchMedia("screen and (min-width: 790px)").matches) {
+       me.style.display = "block";
+    }
 };
