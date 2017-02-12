a3a6ac01e8e073a3290cc793146aa3968ba00a98
code_master5 <lauhny.bimalk@gmail.com>
2017-02-12 19:37:34 +0530

    font tag

--- public/javascripts/portfolio.js
+++ public/javascripts/portfolio.js
@@ -1,23 +1,24 @@
 window.onload = function () {
     
-    var trigram = "&#x2623;";
-    var cross = "&#x2692;";
+    var trigram = "/icons/menu.svg";
+    var cross = "/icons/cross-symbol.svg";
     
     var topLinks = document.getElementsByClassName('top-links')[0];
     var nav = document.getElementsByTagName('nav')[0];
     var responsiveIco = document.getElementsByClassName('responsive-expand')[0];
     var me = document.getElementsByClassName('creator')[0];
+    var respImg = document.getElementById('resp-img');
     
-    document.getElementsByClassName('responsive-expand')[0].addEventListener('click', function () {
+    responsiveIco.addEventListener('click', function () {
         
         if (topLinks.style.display === "none") {
             topLinks.style.display = "inline-block";
-            responsiveIco.innerHTML = cross;
+            respImg.style.url = cross; 
             nav.style.height = "auto"
             me.style.display = "none"
         } else {
             topLinks.style.display = "none";
-            responsiveIco.innerHTML = trigram;
+            respImg.style.url = trigram;
             nav.style.height = "80px";
             me.style.display = "block"
         }
