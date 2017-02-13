6852d4df28d62095bceefd1eabf7b43a59377fc0
code_master5 <lauhny.bimalk@gmail.com>
2017-02-13 22:28:12 +0530

    Corrected NavBar

--- public/javascripts/portfolio.js
+++ public/javascripts/portfolio.js
@@ -1,41 +1,23 @@
 var trigram = "/icons/trigram.png";
 var cross = "/icons/cross.png";
 
-var topLinks = document.getElementsByClassName('top-links')[0];
 var nav = document.getElementsByTagName('nav')[0];
-var responsiveIco = document.getElementsByClassName('responsive-expand')[0];
-var me = document.getElementsByClassName('creator')[0];
-var respImg = document.getElementById('resp-img');
-var menuLi = document.querySelectorAll('.top-links li');
+var topnav = document.getElementById('mytopnav');
+var respIcon = document.getElementsByClassName('resp-icon')[0];
+var respImg = document.getElementsByClassName('resp-img')[0];
 
 window.onload = function () {
     
-    responsiveIco.addEventListener('click', function () {
-        
-        if (topLinks.style.display === "none") {
-            topLinks.style.display = "inline-block";
-            respImg.src = cross; 
-            nav.style.height = "auto"
-            me.style.display = "none"
+    respIcon.addEventListener('click', function () {
+       
+        if (topnav.style.display === "none") {
+            respImg.src = cross;
+            topnav.style.display = "block";
         } else {
-            topLinks.style.display = "none";
             respImg.src = trigram;
-            nav.style.height = "80px";
-            me.style.display = "block"
+            topnav.style.display = "none";
         }
     });
     
     
 };
-
-/*
-window.matchMedia("screen and (min-width: 790px)").addListener(function() {
-    console.log("It's working!");
-    respImg.src = trigram;
-    me.style.display = "block";
-    topLinks.style.display = "block";
-    for (li of menuLi) {
-        li.style.display = "inline";
-    }
-});
-*/
